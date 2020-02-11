e6821aaeecabed04a674b083f51c48f5c6c7c135
TCPGNL <TCPGNL@gmail.com>
2020-02-11 22:33:16 +0800

    文件上传示例

---
+++ fileupl/src/main/java/com/tcpgnl/fileupl/FileuplApplication.java
@@ -0,0 +1,13 @@
+package com.tcpgnl.fileupl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileuplApplication.class, args);
+    }
+
+}
---
+++ fileupl/src/test/java/com/tcpgnl/fileupl/FileuplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcpgnl.fileupl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileuplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
